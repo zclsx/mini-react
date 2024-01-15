d4728b30988a91c417af8f1f6fd473dbcf0ca160
jiushi21 <zc931325671@gmail.com>
2024-01-15 13:41:17 +0800

    重构代码,实现统一提交

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -21,25 +21,90 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+  root = nextWorkOfUnit;
+}
 
-  //id class
-  Object.keys(el.props).forEach((key) => {
+let root = null;
+let nextWorkOfUnit = null;
+function workLoop(deadline) {
+  let shoudYied = false;
+  while (!shoudYied && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit);
+
+    shoudYied = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+function createDom(type) {
+  return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function updatedProps(dom, props) {
+  //2.处理props
+  Object.keys(props).forEach((key) => {
     if (key !== "children") {
-      dom[key] = el.props[key];
+      dom[key] = props[key];
     }
   });
-
-  const children = el.props.children;
-  children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.append(dom);
 }
+
+function initChildren(work) {
+  let preChild = null;
+  //3.链表转换 设置好指针
+  const children = work.props.children;
+  children.forEach((child, index) => {
+    const newWork = {
+      dom: null,
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: work,
+      sibling: null,
+    };
+
+    if (index === 0) {
+      work.child = newWork;
+    } else {
+      preChild.sibling = newWork;
+    }
+    preChild = newWork;
+  });
+}
+
+function performWorkOfUnit(work) {
+  //1.创建dom
+  if (!work.dom) {
+    const dom = (work.dom = createDom(work.type));
+
+    work.parent.dom.append(dom);
+    updatedProps(dom, work.props);
+  }
+
+  initChildren(work);
+
+  //4.返回下一个要执行的任务
+  if (work.child) {
+    return work.child;
+  }
+
+  if (work.sibling) {
+    return work.sibling;
+  }
+
+  return work.parent?.sibling;
+}
+
+requestIdleCallback(workLoop);
 
 const React = {
   createElement,
